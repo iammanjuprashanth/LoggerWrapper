5e4b291630f84bb3b0af9ec0f71e58ee2cce763c
manjuprashanth s <>
2023-12-20 22:07:01 +0530

    test

---
+++ StringInterpolation/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  StringInterpolation
+//
+//  Created by manjuprashanth s on 20/12/23.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
